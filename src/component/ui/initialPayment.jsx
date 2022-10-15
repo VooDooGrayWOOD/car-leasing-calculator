7336007530d946e69cd026d9b3891638a01b4101
VooDooGrayWOOD <voodoograywood@gmail.com>
2022-10-15 09:11:06 +0700

    change initialPayment

--- src/component/ui/initialPayment.jsx
+++ src/component/ui/initialPayment.jsx
@@ -1,26 +1,39 @@
 import React from "react";
 import Range from "../common/range";
+import "./initialPayment.css";
 
 const InitialPayment = () => {
   const data = {
-    defaultValue: "420000",
+    defaultValue: "13",
     min: "10",
-    max: "60"
-  }
+    max: "60",
+  };
 
-  // const finalData = 
+  // const finalData =
 
   return (
     <div>
-        <Range 
-          className="initialPayment" 
-          min={data.min} 
-          max={data.max} 
-          defaultValue={data.defaultValue} 
+      <form>
+        <Range
+          className="initialPayment"
+          min={data.min}
+          max={data.max}
+          defaultValue={data.defaultValue}
           label="Первоначальный взнос"
         />
+        <input
+          className="percentInitialPayment"
+          min={data.min}
+          max={data.max}
+          defaultValue={`${data.defaultValue}%`}
+        />
+      </form>
     </div>
   );
 };
 
 export default InitialPayment;
+
+// <table>
+//     <tr><th><center><input type='text' value='1'/></th><th><div id='pic_1'><img width=20 height=20 src='images/check1.png'/></img></div></th></tr>
+// </table>
--- src/component/ui/leaseTerm.jsx
+++ src/component/ui/leaseTerm.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Range from "../common/range";
 
 const LeaseTerm = () => {
+    const data = {
+        defaultValue: "60",
+        min: "1",
+        max: "60"
+    }
   return (
     <div>
-      <p>Срок лизинга</p>
+      <Range min={data.min} max={data.max} label="Срок лизинга" defaultValue={data.defaultValue}/>
     </div>
   );
 };
